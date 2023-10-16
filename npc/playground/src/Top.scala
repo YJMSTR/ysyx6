57fc589c914078183930a116fa8269a2d428d40a
张炀杰 <jay1273062855@outlook.com>
2023-10-16 16:07:40 +0800

    流水灯

--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -1,12 +1,19 @@
 import chisel3._
-
+import chisel3.util._
 
 class Top extends Module {
   val io = IO(new Bundle {
-  val a  = Input(Bool())
-  val b  = Input(Bool())
-  val f  = Output(Bool())
+    // val clk = Input(Clock())
+    // val rst = Input(Bool())
+    val led  = Output(UInt(16.W))
   })
+  val count = RegInit(0.U(32.W))
+  val led = RegInit(1.U(16.W))
 
-  io.f := (io.a ^ io.b)
+  when(count === 0.U) {
+    led := Cat(led(14, 0), led(15))
+  }
+  count := Mux(count >= 5000000.U, 0.U, count+1.U)
+
+  io.led := led
 }
