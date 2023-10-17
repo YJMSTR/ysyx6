20fe544329b3a5b722394286af5b97c44aa3afe7
张炀杰 <jay1273062855@outlook.com>
2023-10-17 15:38:46 +0800

    数电实验2

--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -3,18 +3,46 @@ import chisel3.util._
 
 class Top extends Module {
   val io = IO(new Bundle {
-    // val clk = Input(Clock())
-    val Y     = Input(UInt(2.W))
-    // val rst = Input(Bool())
-    val X0    = Input(UInt(2.W))
-    val X1    = Input(UInt(2.W))
-    val X2    = Input(UInt(2.W))
-    val X3    = Input(UInt(2.W))
-    val F     = Output(UInt(2.W))
+    val data = Input(Vec(8, Bool()))
+    val Y    = Output(Bool())
+    val out  = Output(UInt(3.W))
+    val seg0 = Output(UInt(7.W))
   })
-  io.F := MuxLookup(io.Y, io.X0, Array(
-    1.U->io.X1,
-    2.U->io.X2,
-    3.U->io.X3
-  ))
+  val tmp = io.data.asUInt
+  io.Y   := tmp.orR
+  val encoded = Wire(UInt(3.W))
+  encoded := 0.U
+  for (i <- 0 until 8) {
+    when (io.data(i)) {
+      encoded := i.U 
+    }
+  }
+  io.seg0 := "b1111111".U
+  switch (encoded) {
+    is (0.U) {
+      io.seg0 := "b0000001".U
+    }
+    is (1.U) {
+      io.seg0 := "b1001111".U
+    }
+    is (2.U) {
+      io.seg0 := "b0010010".U 
+    }
+    is (3.U) {
+      io.seg0 := "b0000110".U 
+    }
+    is (4.U) {
+      io.seg0 := "b1001100".U 
+    }
+    is (5.U) {
+      io.seg0 := "b0100100".U 
+    }
+    is (6.U) {
+      io.seg0 := "b0100000".U
+    }
+    is (7.U) {
+      io.seg0 := "b0001111".U
+    }
+  }
+  io.out := encoded
 }
