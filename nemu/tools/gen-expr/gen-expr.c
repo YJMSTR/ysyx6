95aa0e4b879f9468afabc097fba9f0c2707073aa
张炀杰 <jay1273062855@outlook.com>
2023-10-25 21:32:00 +0800

    测试表达式求值，修改main之前

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -19,9 +19,12 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+// #include <utils.h>
+
 
 // this should be enough
 static char buf[65536] = {};
+static int buf_pointer = 0;
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
@@ -31,8 +34,77 @@ static char *code_format =
 "  return 0; "
 "}";
 
+
+
+ uint32_t tmp_stk[33];
+
+static void gen(char c) {
+	buf[buf_pointer++] = c;
+	buf[buf_pointer] = '\0';
+}
+
+static void gen_num() {
+	uint32_t res = (uint32_t)rand()%32768u;
+	//printf("rand == %u\n", res);
+	int tmp = rand()%7;	//1/7的概率生成负数
+	if (tmp == 0) {
+		gen('(');
+		gen('-');
+	}
+	//if (rand() & 1) gen('-');
+	uint32_t i = 0;
+	while (res) {
+		tmp_stk[++i] = res % 10;
+		res /= 10;
+	}
+	if (i==0) {
+		buf[buf_pointer++] = '0';
+		gen('u');
+		buf[buf_pointer] = '\0';
+		return;
+	}
+	while (i) {
+		buf[buf_pointer++] = tmp_stk[i--]+'0';
+	}
+  gen('u'); 
+	if(tmp == 0) gen(')');
+}
+
+
+
+static void gen_rand_op() {
+	switch (rand()%4) {
+		case 0: gen('+'); break;
+		case 1: gen('-'); break;
+		case 2: gen('*'); break;
+		default: gen('/'); break;
+	}
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  if (rand() & 1) gen(' ');
+  if (buf_pointer >= 40000) {
+    gen_num();
+    if (rand()&1) gen(' ');
+		buf[buf_pointer] = '\0';
+		return;
+  }
+  switch (rand()%3) {
+  	case 0: gen_num(); break;
+  	case 1: gen('('); gen_rand_expr(); gen(')'); break;
+  	default: 
+  		gen_rand_expr(); 
+  		gen_rand_op(); 
+      if (buf[buf_pointer-1]=='/') {
+        gen('(');
+        gen_rand_expr();
+        gen(')');
+        break;
+      }
+  		gen_rand_expr(); break;
+  }
+  if (rand()&1) gen(' ');
+  buf[buf_pointer]='\0';
 }
 
 int main(int argc, char *argv[]) {
@@ -44,17 +116,22 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+POS:
+    buf_pointer = 0;
+    buf[0] = '\0';
     gen_rand_expr();
-
+    //printf("buf = %s\n", buf);
     sprintf(code_buf, code_format, buf);
-
+    //printf("code_buf = %s\n", code_buf);
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    int ret = system("gcc /tmp/.code.c -Wall -Werror -o  /tmp/.expr");
+    if (ret != 0) {
+      goto POS;
+    }
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
@@ -62,7 +139,6 @@ int main(int argc, char *argv[]) {
     int result;
     ret = fscanf(fp, "%d", &result);
     pclose(fp);
-
     printf("%u %s\n", result, buf);
   }
   return 0;
