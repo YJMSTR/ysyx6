6409787ce35bdd2e1896c2e510a214ff3896c073
张炀杰 <jay1273062855@outlook.com>
2024-07-05 14:42:11 +0800

    [v2.0] add difftest for PA

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,14 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0; i < 32; i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      printf("difftest: reg #%d = %s err at pc: "FMT_WORD"\n", i, reg_name(i), pc);
+      printf("difftest: ref_r->gpr[%d] == "FMT_WORD"\n", i, ref_r->gpr[i]);
+      return false;
+    }
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
