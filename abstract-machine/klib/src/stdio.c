f00509bd1f4f46c1627e4fa327f5f05e76899010
张炀杰 <jay1273062855@outlook.com>
2024-07-05 10:43:41 +0800

    [v2.0] pass string in cpu-tests

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,14 +6,137 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+  // putstr("ptf:");
+  char buf[160];
+  va_list args;
+  va_start(args, fmt);
+  int ret = vsprintf(buf, fmt, args);
+  va_end(args);
+  for (int i = 0; i < ret; i++) {
+    putch(buf[i]);
+  }
+  return ret;
   panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
+  uint32_t len = strlen(fmt);
+  int cur = 0;
+  // putstr("vsptf");
+  for (int i = 0; i < len; i++) {
+    if (fmt[i] == '%') {
+      if (i + 1 < len) {
+        switch (fmt[i+1]) {
+          case 'd': 
+            int tmp = va_arg(ap, int);
+            char stk[100];
+            int stk_top = 0, f = 1;
+            if (tmp == 0) {
+              out[cur++] = '0';
+              i++;
+              break;
+            }
+            while (tmp != 0) {
+              if (tmp < 0) {
+                f = -1;
+                tmp = -tmp;
+              }
+              stk[++stk_top] = (tmp % 10) + '0';
+              tmp /= 10;
+            }
+            if (f == -1) out[cur++] = '-';
+            while (stk_top > 0) {
+              out[cur++] = stk[stk_top];
+              stk_top--; 
+            }
+            i++;
+            break;
+          case 's':
+            char *tmpstr = va_arg(ap, char*);
+            int j = 0;
+            while (tmpstr[j] != '\0') {
+              out[cur++] = tmpstr[j];
+              j++;
+            }
+            i++;
+            break;
+          case 'x':
+            unsigned int tmpx = va_arg(ap, int);
+            char stkx[100];
+            unsigned int stkx_top = 0;
+            if (tmpx == 0) {
+              out[cur++] = '0';
+              i++;
+              break;
+            }
+            while (tmpx != 0) {
+              char res = 'A';
+              if (tmpx % 16 > 9) {
+                switch (tmpx % 16)
+                {
+                case 11:
+                  res = 'B';
+                  break;
+                case 12:
+                  res = 'C';
+                  break;
+                case 13:
+                  res = 'D';
+                  break;
+                case 14:
+                  res = 'E';
+                  break;
+                case 15:
+                  res = 'F';
+                  break;
+                default:
+                  res = 'A';
+                  break;
+                }
+              } else {
+                res = (tmpx % 16) + '0';
+              }
+              stkx[++stkx_top] = res;
+              tmpx >>= 4;
+            }
+            if (stkx_top == 0) {
+              out[cur++] = '0';
+            }
+            while (stkx_top > 0) {
+              out[cur++] = stkx[stkx_top];
+              stkx_top--; 
+            }
+            i++;
+            break;
+        }
+      }
+      continue;
+    } else {
+      out[cur++] = fmt[i];
+    } 
+  }  
+  out[cur] = '\0';
+  return cur;
   panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  /*
+  Some programs imprudently rely on code such as the following
+
+      sprintf(buf, "%s some further text", buf);
+
+  to append text to buf.  However, the standards explicitly note that the results are undefined if source and destination buffers overlap when  calling  sprintf(),  snprintf(),
+  vsprintf(), and vsnprintf().  Depending on the version of gcc(1) used, and the compiler options employed, calls such as the above will not produce the expected results.
+
+  The  glibc implementation of the functions snprintf() and vsnprintf() conforms to the C99 standard, that is, behaves as described above, since glibc version 2.1.  Until glibc
+  2.0.6, they would return -1 when the output was truncated.
+  */
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
   panic("Not implemented");
 }
 
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,46 +1,129 @@
 #include <klib.h>
 #include <klib-macros.h>
+
 #include <stdint.h>
 
+
+#include <stdio.h>
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t res = 0;
+  while (s[res] != '\0') {
+    res++;
+  }
+  return res;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  for (int i = 0; i < strlen(src); i++) {  //注意拷贝末尾的'\0'
+    dst[i] = src[i];
+  }
+  dst[strlen(src)] = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++) 
+    dst[i] = src[i];
+  for ( ; i <= n; i++) 
+    dst[i] = '\0';
+  return dst;
 }
 
-char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+char *
+strcat(char *dest, const char *src)
+{
+  size_t dest_len = strlen(dest);
+  size_t i;
+
+  for (i = 0 ; src[i] != '\0' ; i++)
+      dest[dest_len + i] = src[i];
+  dest[dest_len + i] = '\0';
+
+  return dest;
 }
+
+// int strcmp(const char *s1, const char *s2) {
+//   size_t n = strlen(s1);
+//   size_t m = strlen(s2);
+//   for (size_t i = 0; i < (n > m ? m : n); i++) {
+//     if (s1[i] < s2[i]) return -1;
+//     if (s1[i] > s2[i]) return 1;
+//   }
+//   if (n == m) return 0;
+//   return n < m ? -1 : 1;
+// }
 
 int strcmp(const char *s1, const char *s2) {
+  unsigned char c1, c2;
+  while (1) {
+    c1 = *s1++;
+    c2 = *s2++;
+    if (c1 != c2) return c1 < c2 ? -1 : 1;
+    if (!c1) break;
+  }
+  return 0;
   panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t l1 = strlen(s1);
+  size_t l2 = strlen(s2);
+  size_t l = l1 > l2 ? l2 : l1;
+  for (size_t i = 0; i < (n > l ? l : n); i++) {
+    if (s1[i] < s2[i]) return -1;
+    if (s1[i] > s2[i]) return 1;
+  }
+  if (l < n) {
+    if (l1 == l2) return 0;
+    return l1 < l2 ? -1 : 1;
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  if (s == NULL || n < 0) {
+    return NULL;
+  }
+  unsigned char *tmp = (unsigned char *)s;
+  for (size_t i = 0; i < n; i++) {
+    *tmp = c;
+    tmp ++;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  char tmp[n];
+  char *srct = (char *)src;
+  char *dstt = (char *)dst; 
+  for (size_t i = 0; i < n; i++) tmp[i] = srct[i];
+  for (size_t i = 0; i < n; i++) dstt[i] = tmp[i];
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  // n = n / 8;
+  // uint64_t *srct = (uint64_t *)in;
+  // uint64_t *dstt = (uint64_t *)out;
+  uint8_t *srct = (uint8_t *)in;
+  uint8_t *dstt = (uint8_t *)out;
+  for (size_t i = 0; i < n; i++) dstt[i] = srct[i];
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
+  unsigned char *c1 = (unsigned char *)s1;
+  unsigned char *c2 = (unsigned char *)s2;
+  for (size_t i = 0; i < n; i++) {
+    if (c1[i] < c2[i]) return -1;
+    if (c1[i] > c2[i]) return 1;
+  }
+  //panic("return 0");
+  return 0;
   panic("Not implemented");
 }
 
